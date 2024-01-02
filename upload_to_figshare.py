3624dac41803c4c27a047247da7800216bdd252f
Raul Ariza Lopez <ra.arilopez@alum.uca.es>
2024-01-02 20:05:30 +0000

    Update file upload_to_figshare.py

--- upload_to_figshare.py
+++ upload_to_figshare.py
@@ -1,29 +1,20 @@
-from __future__ import print_function
-import subprocess
-import sys
+import os
+from figshare.api import FilesApi
+from figshare.rest import ApiException
 from pprint import pprint
 
-# Instalar las dependencias necesarias
-subprocess.run(["pip", "install", "figshare"])
-
-# Agregar la ruta del paquete figshare al sys.path
-sys.path.append(sys.path[0] + '/venv/lib/python3.*/site-packages')
-
-import swagger_client
-from swagger_client.rest import ApiException
-
 # Configure OAuth2 access token for authorization: OAuth2
-swagger_client.configuration.access_token = 'YOUR_ACCESS_TOKEN'
+os.environ['FIGSHARE_API_KEY'] = '52eae7844e5a73ad33b324d61131c76d4fbc1477ad6ec9813ea5c749845f2a6e7d5eb16bf28f650835fb560a8e10dcb5d9a863c6491caed5615b1736197e3a52'
 
 # Ruta del archivo PNG que deseas subir
-file_path = 'path/to/your/file.png'
+file_path = './grafica_sensor.png'
 
 # Nombre y descripción del artículo en Figshare
 article_title = 'Título de tu artículo en Figshare'
 article_description = 'Descripción de tu artículo en Figshare'
 
 # Crear instancia de la API
-api_instance = swagger_client.FilesApi()
+api_instance = FilesApi()
 
 try:
     # Subir el archivo al artículo privado
