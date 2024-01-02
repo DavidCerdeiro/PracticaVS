3277057abe65bd7aa6b9c90bed5b5c0d96144551
Raul Ariza Lopez <ra.arilopez@alum.uca.es>
2024-01-02 15:48:03 +0000

    Update file grafico.py

--- grafico.py
+++ grafico.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 # Cargar datos desde CSV
 df = pd.read_csv('SensorData.csv')
 
-plt.plot(df['timestamp'], df['temperatureSHT31'], label='Temperatura')
-plt.plot(df['timestamp'], df['humiditySHT31'], label='Humedad')
+plt.plot(df['Timestamp'], df['Temperature'], label='Temperatura')
+plt.plot(df['Timestamp'], df['Humidity'], label='Humedad')
 
 plt.title('Datos del Sensor')
 plt.xlabel('Tiempo')
@@ -13,5 +14,8 @@ plt.ylabel('Valor')
 plt.legend()
 plt.grid(True)
 
-# Guardar grafica
-plt.savefig('grafica_sensor.png')
+# Obtener la ruta al directorio del script
+script_directory = os.path.dirname(os.path.realpath(__file__))
+
+# Guardar la gráfica en el directorio superior
+plt.savefig(os.path.join(script_directory, '..', 'grafica_sensor.png'))
