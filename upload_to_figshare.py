d4c74a58130df1c65b7a6a9f8c5878ffe387a089
Raul Ariza Lopez <ra.arilopez@alum.uca.es>
2024-01-02 20:41:27 +0000

    Update file upload_to_figshare.py

--- upload_to_figshare.py
+++ upload_to_figshare.py
@@ -2,9 +2,9 @@ import os
 import requests
 from pprint import pprint
 
-# Configure OAuth2 access token for authorization: OAuth2
-access_token = '52eae7844e5a73ad33b324d61131c76d4fbc1477ad6ec9813ea5c749845f2a6e7d5eb16bf28f650835fb560a8e10dcb5d9a863c6491caed5615b1736197e3a52'
-headers = {'Authorization': f'Bearer {access_token}'}
+# Configure OAuth token for authorization
+oauth_token = '52eae7844e5a73ad33b324d61131c76d4fbc1477ad6ec9813ea5c749845f2a6e7d5eb16bf28f650835fb560a8e10dcb5d9a863c6491caed5615b1736197e3a52'
+headers = {'Authorization': f'Bearer {oauth_token}'}
 
 # Ruta del archivo PNG que deseas subir
 file_path = './grafica_sensor.png'
@@ -15,7 +15,7 @@ article_description = 'Descripción de tu artículo en Figshare'
 
 # URL de la API de Figshare para subir un archivo
 upload_url = 'https://api.figshare.com/v2/account/articles/24926016/files'
-# Reemplaza <ARTICLE_ID> con el ID del artículo al que deseas adjuntar el archivo
+# Reemplaza '24926016' con el ID real de tu artículo
 
 # Realizar la solicitud para obtener la URL de carga del archivo
 try:
@@ -28,6 +28,7 @@ try:
     file_url = upload_data.get('upload_url')
 except requests.exceptions.RequestException as e:
     print(f"Error al obtener la URL de carga: {e}")
+    print(f"Detalles del error: {response.text}")
     exit(1)
 
 # Subir el archivo al artículo usando la URL obtenida
@@ -37,6 +38,7 @@ try:
         response.raise_for_status()
 except requests.exceptions.RequestException as e:
     print(f"Error al subir el archivo: {e}")
+    print(f"Detalles del error: {response.text}")
     exit(1)
 
 print("Archivo subido exitosamente.")
