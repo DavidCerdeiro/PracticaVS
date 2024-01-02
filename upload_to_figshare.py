965460ba2e5835c4161afde8d2250eb8bc099c13
Raul Ariza Lopez <ra.arilopez@alum.uca.es>
2024-01-02 18:15:34 +0000

    Update file upload_to_figshare.py

--- upload_to_figshare.py
+++ upload_to_figshare.py
@@ -1,15 +1,22 @@
 import figshare
-import requests
 
 def upload_image(api_token, file_path):
-    # Utiliza APIClient en lugar de Figshare
-    client = figshare.APIClient(api_token=api_token)
+    # Create a Figshare client
+    client = figshare.Figshare(api_token=api_token)
 
-    # Carga el archivo en Figshare
-    article = client.file_upload(file_path)
+    # Create a new article (data structure to store your files)
+    article = client.article()
 
-    # Imprime el enlace al artículo creado
-    print("File uploaded successfully. Article URL:", article['location'])
+    # Add a file to the article
+    article.add_file(file_path)
 
-# Llama a la función de carga de imágenes
-upload_image(api_token="64d223981d339c71a4a242888220713bbeb16509ddfbedc5eb6c50bd3b5b7da01d0b7a5faf9904d779ddbf19513e3bdc99a87e26a878531efc6956ad09fcb708", file_path="./venv/grafica_sensor.png")
+    # Save the article
+    article.save()
+
+    # Publish the article (make it publicly accessible)
+    article.publish()
+
+if __name__ == "__main__":
+    # Replace 'your_api_token' with your actual Figshare API token
+    upload_image(api_token="64d223981d339c71a4a242888220713bbeb16509ddfbedc5eb6c50bd3b5b7da01d0b7a5faf9904d779ddbf19513e3bdc99a87e26a878531efc6956ad09fcb708",
+                 file_path="./venv/grafica_sensor.png")
