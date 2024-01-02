19be02f92e9daf128524942822391b1f3fa377d0
Raul Ariza Lopez <ra.arilopez@alum.uca.es>
2024-01-02 18:10:07 +0000

    Update file upload_to_figshare.py

--- upload_to_figshare.py
+++ upload_to_figshare.py
@@ -1,18 +1,15 @@
-# Contenido de upload_to_figshare.py
-
 import figshare
+import requests
 
 def upload_image(api_token, file_path):
-    # Configurar el cliente Figshare
-    client = figshare.Figshare(api_token=api_token)
+    # Utiliza APIClient en lugar de Figshare
+    client = figshare.APIClient(api_token=api_token)
 
-    # Crear un nuevo artículo en Figshare
-    article = client.create_article(title="Graph Image", description="Uploaded from GitLab CI")
+    # Carga el archivo en Figshare
+    article = client.file_upload(file_path)
 
-    # Subir la imagen al artículo
-    file_id = client.upload_file_to_article(article_id=article['id'], file_path=file_path)
+    # Imprime el enlace al artículo creado
+    print("File uploaded successfully. Article URL:", article['location'])
 
-    print(f"Image uploaded to Figshare with file ID: {file_id}")
-
-# Usar la función con el token de la API de Figshare y la ruta de la imagen
+# Llama a la función de carga de imágenes
 upload_image(api_token="64d223981d339c71a4a242888220713bbeb16509ddfbedc5eb6c50bd3b5b7da01d0b7a5faf9904d779ddbf19513e3bdc99a87e26a878531efc6956ad09fcb708", file_path="./venv/grafica_sensor.png")
