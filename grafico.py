7929e52adb3bdb44b7355d8fba40301f66feb6a6
Raul Ariza Lopez <ra.arilopez@alum.uca.es>
2024-01-02 16:07:37 +0000

    Update file grafico.py

--- grafico.py
+++ grafico.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
 
 # Cargar datos desde CSV
 df = pd.read_csv('SensorData.csv')
@@ -14,8 +13,5 @@ plt.ylabel('Valor')
 plt.legend()
 plt.grid(True)
 
-# Obtener la ruta al directorio del script
-script_directory = os.path.dirname(os.path.realpath(__file__))
-
-# Guardar la gráfica en el directorio superior
-plt.savefig(os.path.join(script_directory, '..', 'grafica_sensor.png'))
+# Guardar grafica
+plt.savefig('grafica_sensor.png')
