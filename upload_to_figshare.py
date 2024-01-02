730419a7d9f3d7f5244420a3bc923c4d1f029736
Raul Ariza Lopez <ra.arilopez@alum.uca.es>
2024-01-02 19:50:26 +0000

    Update file upload_to_figshare.py

--- upload_to_figshare.py
+++ upload_to_figshare.py
@@ -1,9 +1,16 @@
 import os
-import sys
+import subprocess
 
-# Agregar el directorio figshare_api al path para importar Figshare
-sys.path.append(os.path.join(os.path.dirname(__file__), "venv/lib/python3.*/site-packages"))
+# Definir las variables necesarias
+api_key = os.environ.get("FIGSHARE_APP_ID")
+file_path = "venv/grafica_sensor.png"  # Asegúrate de que la ruta sea correcta
+article_title = "Título del Artículo en Figshare"
+article_description = "Descripción del Artículo en Figshare"
 
+# Instalar Figshare
+subprocess.run(["pip", "install", "figshare"])
+
+# Importar Figshare después de la instalación
 from figshare.figshare_api import Figshare
 
 def upload_to_figshare(api_key, file_path, article_title, article_description):
@@ -16,12 +23,5 @@ def upload_to_figshare(api_key, file_path, article_title, article_description):
     # Subir el archivo al artículo
     article.add_file(file_path)
 
-if __name__ == "__main__":
-    # Definir las variables necesarias
-    api_key = os.environ.get("FIGSHARE_APP_ID")
-    file_path = "venv/grafica_sensor.png"  # Asegúrate de que la ruta sea correcta
-    article_title = "Título del Artículo en Figshare"
-    article_description = "Descripción del Artículo en Figshare"
-
-    # Llamar a la función para subir a Figshare
-    upload_to_figshare(api_key, file_path, article_title, article_description)
+# Llamar a la función para subir a Figshare
+upload_to_figshare(api_key, file_path, article_title, article_description)
