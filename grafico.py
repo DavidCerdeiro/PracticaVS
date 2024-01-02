54ac24fca2d3224ccf5320c686ea53d067af94da
Raul Ariza Lopez <ra.arilopez@alum.uca.es>
2024-01-02 15:52:30 +0000

    Update file grafico.py

--- grafico.py
+++ grafico.py
@@ -5,8 +5,8 @@ import os
 # Cargar datos desde CSV
 df = pd.read_csv('SensorData.csv')
 
-plt.plot(df['Timestamp'], df['Temperature'], label='Temperatura')
-plt.plot(df['Timestamp'], df['Humidity'], label='Humedad')
+plt.plot(df['timestamp'], df['temperatureSHT31'], label='Temperatura')
+plt.plot(df['timestamp'], df['humiditySHT31'], label='Humedad')
 
 plt.title('Datos del Sensor')
 plt.xlabel('Tiempo')
