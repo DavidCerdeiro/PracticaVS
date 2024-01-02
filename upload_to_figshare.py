5c170c14c25496c6149f45228e1cfa51f1b41824
Raul Ariza Lopez <ra.arilopez@alum.uca.es>
2024-01-02 17:44:00 +0000

    Update file upload_to_figshare.py

--- upload_to_figshare.py
+++ upload_to_figshare.py
@@ -5,10 +5,9 @@ def upload_to_figshare(file_path, figshare_token, article_id):
     # Configurar la URL de la API de Figshare para cargar archivos en un artículo específico
     upload_url = f'https://api.figshare.com/v2/account/articles/{article_id}/files'
 
-    # Configurar el encabezado con el token de Figshare y el tipo de contenido
+    # Configurar el encabezado con el token de Figshare
     headers = {
-        'Authorization': f'token {figshare_token}',
-        'Content-Type': 'image/png',
+        'Authorization': f'token {figshare_token}'
     }
 
     # Cargar el archivo al artículo de Figshare
@@ -26,7 +25,7 @@ def upload_to_figshare(file_path, figshare_token, article_id):
 # Configurar las variables necesarias
 figshare_token = '64d223981d339c71a4a242888220713bbeb16509ddfbedc5eb6c50bd3b5b7da01d0b7a5faf9904d779ddbf19513e3bdc99a87e26a878531efc6956ad09fcb708'
 article_id = '24926016'
-file_path = './grafica_sensor.png'
+file_path = './venv/grafica_sensor.png'
 
 # Llamar a la función para subir el archivo
 upload_to_figshare(file_path, figshare_token, article_id)
